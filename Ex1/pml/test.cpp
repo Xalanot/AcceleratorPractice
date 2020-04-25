ad7d2e464eaa8cb9960fd93936997d672fd02e70
Benjamin Maier <Benjamin@vpn156c.rzuser.uni-heidelberg.de>
2020-04-25 12:38:21 +0200

    add move

--- Ex1/pml/test.cpp
+++ Ex1/pml/test.cpp
@@ -7,7 +7,7 @@ int main()
     CSVWriter csvwriter("test.csv");
     
     std::vector<std::string> headerNames{"sizes", "time"};
-    csvwriter.setHeaderNames(headerNames);
+    csvwriter.setHeaderNames(std::move(headerNames));
     
     std::vector<int> sizes = {10, 20};
     std::vector<float> times = {2.4, 8.9};
