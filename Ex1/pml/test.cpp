daeed7af3dc22d4c554d047fc14df4cc1dd11f29
Benjamin Maier <Benjamin@vpn156c.rzuser.uni-heidelberg.de>
2020-04-25 12:47:38 +0200

    add measurement

--- Ex1/pml/test.cpp
+++ Ex1/pml/test.cpp
@@ -1,16 +1,28 @@
+#include <chrono>
 #include <iostream>
+#include <thread>
+
 #include "csvwriter.h"
 #include "measurement.h"
 
 int main()
 {
-    CSVWriter csvwriter("test.csv");
+    CSVWriter csvwriter("measurment.csv");
     
     std::vector<std::string> headerNames{"sizes", "time"};
     csvwriter.setHeaderNames(std::move(headerNames));
     
-    std::vector<int> sizes = {10, 20};
-    std::vector<float> times = {2.4, 8.9};
+    std::vector<int> sizes {1, 2};
+    std::vector<Measurement<std::chrono::seconds>> times;
+
+    for (auto const& ele : sizes)
+    {
+        Measurement measurement;
+        measurement.start();
+        std::this_thread::sleep_for(std::chrono::seconds(ele));
+        measurement.stop();
+        times.push_back(std::move(measurement));
+    }
 
     csvwriter.write(sizes, times);
 
