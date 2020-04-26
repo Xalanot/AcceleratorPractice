04471c826a92180cd5197bbb83ea509ec7fec4fe
Benjamin Maier <Benjamin@vpn193-051.rzuser.uni-heidelberg.de>
2020-04-26 17:26:16 +0200

    fix sparse

--- Ex2/2.2/sparse_vector.cu
+++ Ex2/2.2/sparse_vector.cu
@@ -4,17 +4,29 @@
 #include <thrust/reduce.h>
 #include <thrust/inner_product.h>
 
+#include <chrono>
 #include <iostream>
+#include <string>
 #include <vector>
 
 #include "advanced_sparse.h"
 #include "common_sparse.h"
 #include "simple_sparse.h"
 
+#include "../csvwriter.h"
+#include "../pml/measurement.h"
+
 int main(void)
 {
+    int iterations = 100;
+    std::vector<int> vectorCount;
+    std::vector<MeasurementSeries<std::chrono::microseconds>> oldTimes;
+    std::vector<MeasurementSeries<std::chrono::microseconds>> newTimes;
+
+
     for (int number_vectors = 2; number_vectors <= 10; ++number_vectors)
     {
+        vectorCount.push_back(number_vectors);
         // generate index vectors
         std::vector<thrust::device_vector<int>> indexVectors(number_vectors);
         for (size_t i = 0; i < indexVectors.size(); ++i)
@@ -34,29 +46,44 @@ int main(void)
         }
 
         // use old method
+        MeasurementSeries<std::chrono::microseconds> measurementOld;
         thrust::device_vector<int> result_index_old;
         thrust::device_vector<float> result_value_old;
-        sum_sparse_vectors(indexVectors, valueVectors, result_index_old, result_value_old);
+        for (int i = 0; i < iterations; ++i)
+        {
+            cudaDeviceSynchronize();
+            measurementOld.start();
+            sum_sparse_vectors(indexVectors, valueVectors, result_index_old, result_value_old);
+            cudaDeviceSynchronize();
+            measurementOld.stop();
+        }
+        oldTimes.push_back(measurementOld);
 
         // use new method
-        // use old method
+        MeasurementSeries<std::chrono::microseconds> measurementNew;
         thrust::device_vector<int> result_index_new;
         thrust::device_vector<float> result_value_new;
-        sum_multiple_sparse_vectors(indexVectors, valueVectors, result_index_new, result_value_new);
+        for (int i = 0; i < iterations; ++i)
+        {
+            cudaDeviceSynchronize();
+            measurementNew.start();
+            sum_multiple_sparse_vectors(indexVectors, valueVectors, result_index_new, result_value_new);
+            cudaDeviceSynchronize();
+            measurementNew.stop();
+        }
+        newTimes.push_back(measurementNew);
         
         if (checkResults(result_index_old, result_value_old, result_index_new, result_value_new))
         {
-            std::cout << "result is right" << std::endl;
-        }
-        else 
-        {
-            std::cout << "wrong" << std::endl;
-            std::cout << "old" << std::endl;
-            print_sparse_vector(result_index_old, result_value_old);
-            std::cout << "new" << std::endl;
-            print_sparse_vector(result_index_new, result_value_new);
+            std::cout << "result is wrong" << std::endl;
         }
     }
+
+    CSVWriter csvwriter("sparse.csv")
+    std::vector<std::string> headerNames {"vector count", "old method", "new method"};
+    csvwriter.setHeaderNames(std::move(headerNames));
+
+    csvwriter.write(vectorCount, oldTimes, newTimes);
 
 }
 
