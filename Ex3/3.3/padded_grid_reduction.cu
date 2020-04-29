127bed41fd83fa8b5ba51baad17599a52415a4f3
Benjamin Maier <Benjamin@vpn044c.rzuser.uni-heidelberg.de>
2020-04-29 13:00:26 +0200

    add padded grid

--- Ex3/3.3/padded_grid_reduction.cu
+++ Ex3/3.3/padded_grid_reduction.cu
@@ -154,7 +154,7 @@ int main(void)
   auto zip_iterator_last = thrust::make_zip_iterator(thrust::make_tuple(data.end(), data.end()));
   auto permutation_iterator_first = thrust::make_permutation_iterator(zip_iterator_first, map.begin());
   auto permutation_iterator_last = thrust::make_permutation_iterator(zip_iterator_first, map.end());
-  std::cout << thrust::get<0>(permutation_iterator_first) << std::endl;
+  std::cout << thrust::get<0>(permutation_iterator_first[0]) << std::endl;
   /* reduce_tuple_new<float> binary_op_new;
   result_type result_new = 
     thrust::reduce(
