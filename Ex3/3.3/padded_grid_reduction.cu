7ea0a49904872499fc30f924e895ac4b7de6f7e1
Benjamin Maier <Benjamin@vpn044c.rzuser.uni-heidelberg.de>
2020-04-29 13:12:18 +0200

    add padded grid

--- Ex3/3.3/padded_grid_reduction.cu
+++ Ex3/3.3/padded_grid_reduction.cu
@@ -157,10 +157,7 @@ int main(void)
 
   thrust::tuple<float, float> init_new(FLT_MAX, -FLT_MAX);
   reduce_tuple_new<float> binary_op_new;
-
-  init_new = binary_op_new(permutation_iterator_first[0], permutation_iterator_first[1]);
-  std::cout << thrust::get<1>(init_new) << std::endl;
-  /* result_type result_new = 
+  result_type result_new = 
     thrust::reduce(
         permutation_iterator_first,
         permutation_iterator_last,
@@ -168,7 +165,7 @@ int main(void)
         binary_op_new);
 
   std::cout << "minimum value: " << thrust::get<1>(result_new) << std::endl;
-  std::cout << "maximum value: " << thrust::get<2>(result_new) << std::endl; */
+  std::cout << "maximum value: " << thrust::get<2>(result_new) << std::endl;
     
 
 
