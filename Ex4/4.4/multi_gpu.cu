424fdb1eef0fa2b7af66e3d7536da59b15d121eb
Benjamin Maier <Benjamin@vpn511-210.rzuser.uni-heidelberg.de>
2020-05-02 18:52:26 +0200

    add multi gpu

--- Ex4/4.4/multi_gpu.cu
+++ Ex4/4.4/multi_gpu.cu
@@ -155,7 +155,7 @@ int main(int argc, char **argv)
     int deviceCount;
     checkCudaError(cudaGetDeviceCount(&deviceCount));
 
-    saxpy_multi_vs_single(10000, deviceCount);
+    saxpy_multi_vs_single(10000000, deviceCount);
 
     return 0;
 }
