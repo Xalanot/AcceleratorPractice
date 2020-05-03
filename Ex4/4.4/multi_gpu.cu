32e2fbd28935661439fd1a4681a64136f2a98a2c
Benjamin Maier <Benjamin@vpn191a.rzuser.uni-heidelberg.de>
2020-05-03 10:25:59 +0200

    pinned memory

--- Ex4/4.4/multi_gpu.cu
+++ Ex4/4.4/multi_gpu.cu
@@ -74,6 +74,7 @@ int main(int argc, char **argv)
     checkCudaError(cudaGetDeviceCount(&deviceCount));
 
     //saxpy_multi_vs_single(100000000, deviceCount);
+    norm_multi_vs_single(4, deviceCount);
 
     return 0;
 }
