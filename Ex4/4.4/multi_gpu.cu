b7785c32e16bf9120cf5862f2bc074aeac27b2a6
Benjamin Maier <Benjamin@vpn193-058.rzuser.uni-heidelberg.de>
2020-05-04 07:44:06 +0200

    moving average

--- Ex4/4.4/multi_gpu.cu
+++ Ex4/4.4/multi_gpu.cu
@@ -67,13 +67,10 @@ void simple_moving_average_multi(float *X_h, size_t N, size_t w, float* result, 
 
         checkCudaError(cudaSetDevice(i));
         
-        thrust::device_vector<float>X_d(deviceSize);
-        checkCudaError(cudaMemcpyAsync(thrust::raw_pointer_cast(X_d.data()), X_h + ptrOffset, deviceSize * float_size, cudaMemcpyDefault, deviceManagers[i].h2dStream));
+        thrust::device_vector<float> X_d(deviceSize);
+        checkCudaError(cudaMemcpy(thrust::raw_pointer_cast(X_d.data()), X_h + ptrOffset, deviceSize * float_size, cudaMemcpyDefault));
 
         // wait for copy to complete
-        checkCudaError(cudaEventRecord(deviceManagers[i].copyEvent, deviceManagers[i].h2dStream));
-        cudaStreamWaitEvent(deviceManagers[i].h2dStream, deviceManagers[i].copyEvent, 0);
-        checkCudaError(cudaEventSynchronize(deviceManagers[i].copyEvent));
         if (i == 0)
         {
             for (int j = 0; j < deviceSize; ++j)
