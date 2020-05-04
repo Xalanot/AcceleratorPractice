7447dd127a7173c317960e3a096364da18e5ccd4
Benjamin Maier <Benjamin@vpn193-058.rzuser.uni-heidelberg.de>
2020-05-04 10:55:04 +0200

    moving average

--- Ex4/4.4/multi_gpu.cu
+++ Ex4/4.4/multi_gpu.cu
@@ -84,7 +84,7 @@ void simple_moving_average_multi(float* X_h, size_t N, size_t w, float* result, 
         {
             for (int j = 0; j < resultSize; ++j)
             {
-                if (temp[j] - result_single[j + ptrOffset] > 1e-5)
+                if (temp[j] - result_single[j + resultOffset] > 1e-5)
                 {
                     std::cout << "WRONG" << std::endl;
                     std::cout << "j: " << j + ptrOffset << std::endl;
