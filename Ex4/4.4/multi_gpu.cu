756be12e67ac90ea15e8a39ae53bf8befc9a16ea
Benjamin Maier <Benjamin@vpn193-058.rzuser.uni-heidelberg.de>
2020-05-04 07:23:10 +0200

    moving average

--- Ex4/4.4/multi_gpu.cu
+++ Ex4/4.4/multi_gpu.cu
@@ -79,7 +79,7 @@ void simple_moving_average_multi(float *X_h, size_t N, size_t w, float* result, 
 
         // compute cumulative sum
         thrust::exclusive_scan(thrust::cuda::par.on(deviceManagers[i].transformStream), X_d.begin(), X_d.end(), temp.begin());
-        temp[temp.size()] = X_d.back() + temp[temp.size() - 1];
+        temp[deviceSize] = X_d.back() + temp[deviceSize - 1];
 
         // compute moving averages from cumulative sum
         //thrust::transform(thrust::cuda::par.on(deviceManagers[i].transformStream), temp.begin() + w, temp.end(), temp.begin(), temp.begin(), minus_and_divide<float>(static_cast<float>(w)));
