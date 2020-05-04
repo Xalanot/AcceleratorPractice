82938130181d13ff05cd36c9b0dab281d3cc634d
Benjamin Maier <Benjamin@vpn193-058.rzuser.uni-heidelberg.de>
2020-05-04 06:58:14 +0200

    moving average

--- Ex4/4.4/multi_gpu.cu
+++ Ex4/4.4/multi_gpu.cu
@@ -85,7 +85,7 @@ void simple_moving_average_multi(float *X_h, size_t N, size_t w, float* result, 
         // compute moving averages from cumulative sum
         thrust::transform(thrust::cuda::par.on(deviceManagers[i].transformStream), temp.begin() + w, temp.end(), temp.begin(), temp.begin(), minus_and_divide<float>(static_cast<float>(w)));
 
-        checkCudaError(cudaMemcpy(thrust::raw_pointer_cast(result), thrust::raw_pointer_cast(temp.data()), 1 * float_size, cudaMemcpyDeviceToHost));
+        //checkCudaError(cudaMemcpy(thrust::raw_pointer_cast(result), thrust::raw_pointer_cast(temp.data()), 1 * float_size, cudaMemcpyDeviceToHost));
         /*
         checkCudaError(cudaEventSynchronize(myDevices[i].stop));
         checkCudaError(cudaEventElapsedTime(&myDevices[i].myTime, myDevices[i].start, myDevices[i].stop));
