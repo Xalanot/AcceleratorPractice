050363a8f6831a5330a10ff9a0725563d77ad9d1
Benjamin Maier <Benjamin@vpn115d.rzuser.uni-heidelberg.de>
2020-05-19 15:07:27 +0200

    2.3

--- Ex2/2.3/scan_by_key.cu
+++ Ex2/2.3/scan_by_key.cu
@@ -15,7 +15,7 @@ void print(const Vector& v)
 thrust::device_vector<int> getValueVector(size_t N)
 {
     thrust::default_random_engine rng;
-    thrust::uniform_int_distribution<int> dist(0, 100);
+    thrust::uniform_int_distribution<int> dist(0, 10);
     thrust::device_vector<int> values(N);
     for (size_t i = 0; i < values.size(); ++i)
     {
@@ -49,12 +49,47 @@ thrust::device_vector<int> getFlagVector(size_t N)
     return flags;
 }
 
+struct value_flag_pair
+{
+    int value;
+    int flag;
+};
+
+struct pair_binary_op
+    : public thrust::binary_function<int, int, value_flag_pair>
+{
+    __host__ __device__
+    pair_binary_op operator()(int value, int flag) const
+    {
+        value_flag_pair pair;
+        pair.value = value;
+        pair.flag = flag;
+        return pair;
+    }
+}   
+
+struct scan_binary_op
+    : public thrust::binary_function<value_flag_pair, value_flag_pair, int>
+{
+    __host__ __device__
+    scan_binary_op operator()(value_flag_pair const& first, value_flag_pair const& second) const
+    {
+        return first.value + second.value;
+    }
+}
+
 int main()
 {
     size_t N = 20;
     thrust::device_vector<int> values = getValueVector(N);
     thrust::device_vector<int> flags = getFlagVector(N);
 
-    print(flags);
+    thrust::device_vector<value_flag_pair> pairs(N);
+    thrust::transform(values.begin(), values.end(), flags.begin(), pair_binary_op());
+
+    thrust::device_vector<int> output(N);
+    thrust::inclusive_scan(pairs.begin(), pairs.end(), output.begin(), 0, scan_binary_op());
+
+    print(output);
     return 0;
 }
