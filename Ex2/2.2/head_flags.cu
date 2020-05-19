dbcf1be3a53a1d0b629265c9d679fcded567b623
Benjamin Maier <Benjamin@vpn510-239.rzuser.uni-heidelberg.de>
2020-05-19 07:25:40 +0200

    add head flags

--- Ex2/2.2/head_flags.cu
+++ Ex2/2.2/head_flags.cu
@@ -85,7 +85,7 @@ thrust::device_vector<int> generateFlags(thrust::device_vector<int> const& keys)
 thrust::device_vector<int> generateKeys(thrust::device_vector<int> const& flags)
 {
     thrust::device_vector<int> keys(flags.size());
-    thrust::inclusive_scan(flags.begin(), flags.end(), keys.begin());
+    thrust::inclusive_scan(flags.begin() + 1, flags.end(), keys.begin() + 1);
     return keys;
 }
 
